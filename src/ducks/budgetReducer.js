7ed2ddc7997fa72829182e71e141e571c1f4d138
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-04-24 15:14:55 -0500

    STEP 5 DONE

--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -3,6 +3,8 @@ const initialState = {
     budgetLimit: null,
     loading: false
   }
+
+  const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA'
   
   export default function reducer(state = initialState, action) {
     return state;
